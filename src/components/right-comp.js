84baf6279c12c8c5cf46d685c1480e7faa529d91
The-BigMan-tech <saddlebox9@gmail.com>
2024-11-12 18:49:37 +0100

    Almost done

--- src/components/right-comp.js
+++ src/components/right-comp.js
@@ -3,12 +3,13 @@ import tw from 'tailwind-styled-components'
 import Step1 from './step-1'
 import Step2 from './step-2'
 import Step3 from './step-3'
+import Step4 from './step-4'
 
 const Steps = tw.div`
     relative left-24 top-10
 `
 export default function RightComponent() {
-    let current_step = <Step3/>
+    let current_step = <Step4/>
     return (
         <>
         <Steps>
---
+++ src/components/step-4.js
@@ -0,0 +1,12 @@
+'use client'
+import { StepFlex,Heading,Instruction } from "./step-1" 
+export default function Step4() {
+    return (
+        <>
+        <StepFlex>
+            <Heading>Finishing up</Heading>
+            <Instruction>Double check if everything looks ok before confirming</Instruction>
+        </StepFlex>
+        </>
+    )
+}
